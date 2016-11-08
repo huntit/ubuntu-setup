acdde0fa7c1ad436df59a3654bd8694b889865c8
Peter Hunt <huntit7@gmail.com>
2016-11-09 09:35:35 +1000

    Added FTP firewall rule to virtualmin setup script

--- virtualmin-setup.sh
+++ virtualmin-setup.sh
@@ -3,6 +3,8 @@
 # Script for the setup of a LAMP stack with Virtualmin hosting on a Ubuntu 14.04 LTS server
 
 #Installing Virtualmin (Apache, BIND, Postfix, procmail, SpamAssassin, ClamAV, Dovecot, ProFTPd, MySQL, PostgreSQL, Mailman)
+#Allow ftp through firewall
+sudo ufw allow 21/tcp
 sudo apt-get -y update
 echo "*** Installing Virtualmin ..."
 sudo wget http://software.virtualmin.com/gpl/scripts/install.sh

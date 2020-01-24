014b141df703226dabc63037b984989f321014a0
Peter Hunt <huntit@users.noreply.github.com>
2020-01-24 21:35:33 +1000

    Added --minimal option for virtualmin-setup.sh

--- virtualmin-setup.sh
+++ virtualmin-setup.sh
@@ -3,12 +3,13 @@
 # Script for the setup of a LAMP stack with Virtualmin hosting on a Ubuntu 14.04/16.04 LTS server
 
 #Installing Virtualmin (Apache, BIND, Postfix, procmail, SpamAssassin, ClamAV, Dovecot, ProFTPd, MySQL, PostgreSQL, Mailman)
+#Use --minimal for a minimal install (without SpamAssassin, ClamAV, etc. for lower memory systems)
 #Allow ftp through firewall
 sudo ufw allow 21/tcp
 sudo apt-get -y update
 echo "*** Installing Virtualmin ..."
 sudo wget http://software.virtualmin.com/gpl/scripts/install.sh
 sudo chmod +x install.sh
-sudo ./install.sh
+sudo ./install.sh $1
 
 
